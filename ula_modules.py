954354da3be9e1aecc397433bc0197ea5c7e654c
HenryIdesis <159027095+HenryIdesis@users.noreply.github.com>
2024-09-05 12:27:14 +0000

    Commit 1

--- ula_modules.py
+++ ula_modules.py
@@ -6,27 +6,34 @@
 
 from myhdl import *
 
-
 @block
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
-
+        soma.next = a ^ b
+        carry.next = a and b
     return instances()
-
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+    haList = [None for i in range(2)]  # (1)
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) 
+    haList[1] = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2]
 
     return instances()
 
-
 @block
-def adder2bits(x, y, soma, carry):
+def adder2bits(x, y, soma, vaiUm):
+    c = Signal(bool(0))
+    half = halfAdder(x[0],y[0],soma[0],c)
+
+    full = fullAdder(x[1], y[1], c, soma[1], vaiUm)
     return instances()
 
 
